9d97d536446fb1f5b7380f8a6fb6e07437d64c26
최홍식 <hongsik@letsdingco.com>
2025-09-15 10:13:09 +0900

    design: 댓글 목록 수정, 삭제 아이콘 레이아웃 수정

--- src/components/boards-detail/comment-list/index.tsx
+++ src/components/boards-detail/comment-list/index.tsx
@@ -32,8 +32,7 @@ export default function CommentList() {
     <div className={styles.commentListBody}>
       <div className={styles.commentListContainer}>
         {data?.fetchBoardComments.map((comment, index) => (
-          <div key={comment?._id}>
-            <div className={styles.listBody}>
+            <div className={styles.listBody} key={comment?._id}>
               <div className={styles.listTitle}>
                 <div className={styles.forwardTitle}>
                   <Image
@@ -45,7 +44,6 @@ export default function CommentList() {
                   </div>
                   <Rate defaultValue={comment?.rating} />
                 </div>
-                <div>
                   <div className={styles.backTitle}>
                     <Image
                       src={IMAGE_SRC.editImage.src}
@@ -56,13 +54,11 @@ export default function CommentList() {
                       alt={IMAGE_SRC.closeImage.alt}
                     />
                   </div>
-                </div>
               </div>
               <div className={styles.commentText}>{comment?.contents}</div>
               <div className={styles.commentDate}>
                 {comment?.createdAt.split("T")[0].split("-").join(".")}
               </div>
-            </div>
             {/* 마지막 댓글 밑에는 border 가 없음 */}
             {index + 1 !== data?.fetchBoardComments.length && (
               <div className={styles.border}> </div>
